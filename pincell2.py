1d7d5aef7e641df65a4be13a148598bf1d121e5a
Isaac Meyer <icmeyer@mit.edu>
2021-03-26 12:51:37 -0400

    commit test 2

--- pincell2.py
+++ pincell2.py
@@ -10,7 +10,7 @@ from ray import Ray, make_segments
 
 pitch = 1.26
 circle = Circle(surface_id=1, boundary_type='transmission', x0=pitch/2,
-             y0=pitch/2, R=0.39218) #0.39218
+             y0=pitch/2, R=0.39218) # 0.39218
 left = XPlane(surface_id=2, boundary_type='reflection', x0=0)
 right = XPlane(surface_id=3, boundary_type='reflection', x0=pitch)
 top = YPlane(surface_id=4, boundary_type='reflection', y0=pitch)
